1a674e140a6f322fecbbac1d3c84227dc2f58933
JOHNJINU <jinujohn09@gmail.com>
2021-03-01 02:11:25 +0530

    careers done current job opening remaining

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -1,6 +1,6 @@
 import {Component} from 'react';
 import Footer from './Footer';
-import '../css/About.css';
+import '../css/Careers.css';
 import icCarousel1 from '../img/carousel_img_1.png';
 
 class Careers extends Component{
@@ -8,28 +8,17 @@ class Careers extends Component{
     render(){
         return(
             <div>
-                
-                <p>THis is careers</p>
-
-                {/* top quadrant starts */}
-
-                <div className="topQuadrantFirstBar d-flex" >
-                    <div>
-
+                <h3 className="careerHeading">Careers</h3>                
+                <div className="careerBar">
+                    <img src={icCarousel1} />
+                    <div className="careerBarText">
+                        <p className="careerBarTextContent" >If you believe you can bring more value to our team,</p>
+                        <span className="careerBarTextTagLine">Send your resume to</span>
+                        <span className="careerBarTextMailId">abc@abc.com</span>
+                        <hr className="careerBarTextMailIdUnderLine"/>
                     </div>
-                    <div>
-                        <img src={icCarousel1} />
-                    </div>
+                    
                 </div>
-                <div className="topQuadrantSecondBar d-flex">
-                    <div>
-
-                    </div>
-                </div>
-
-                {/* top quadrant ends */}
-                
-
                 <Footer />
             </div>
         )
