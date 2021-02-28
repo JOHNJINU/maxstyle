5c673d0be42f700f3c17db774a07ab90fe906b3c
JOHNJINU <jinujohn09@gmail.com>
2021-03-01 00:14:10 +0530

    contact us page complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import {Component} from 'react';
 import Footer from './Footer';
 import '../css/Contact.css';
-import icCarousel1 from '../img/carousel_img_1.png';
+import icCarousel1 from '../img/ic_enquiry_bg.png';
 
 import AddressCard from './AddressCard';
 
@@ -11,9 +11,23 @@ class Contact extends Component{
         return(
             <div>
 
+                {/* {top bar starts} */}
+                <div className="topQuadrantFirstBarAbout contactUsTopPortionText d-flex" >
+                    <div className="firstBarHalfAbout">
+
+                        <h4>Contact us</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero accumsan nisi dignissim feugiat. 
+                            Vestibulum mollis, justo sed vulputate fermentum, augue dolor facilisis erat, sed venenatis mi velit ac enim.</p>
+                    </div>
+                    <div className="secondBarHalfAbout">
+                        <img src={icCarousel1} />
+                    </div>
+                </div>
+                {/* {top bar ends} */}
+
                 {/* enquiry section starts */}
                 <div className="enquirySectionContainer">
-                    <img className="imageOne" src={icCarousel1} />
+                    <img className="imageEnquiry" src={icCarousel1} />
                     <div className="typingFieldContainer">
                         <h5>Enquiry</h5>
                         <form>
@@ -25,7 +39,7 @@ class Contact extends Component{
                             <br/>
                             <input type="text" name="projectLocation" placeholder="PROJECT LOCATION"></input>
                             <br/>
-                            <textarea name="message" cols="30" rows="6"  placeholder="MESSAGE"></textarea>
+                            <textarea name="message" cols="30" rows="4"  placeholder="MESSAGE"></textarea>
                             <br/>
                             <input type="submit" value="Submit"/>
                         </form>
