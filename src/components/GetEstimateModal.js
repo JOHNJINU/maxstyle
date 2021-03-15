bda2bcfac4a7a1856f02161995373bc8490d0c5c
JOHNJINU <jinujohn09@gmail.com>
2021-03-15 15:32:00 +0530

    form values console printed next is sending values to php api through axios

--- src/components/GetEstimateModal.js
+++ src/components/GetEstimateModal.js
@@ -6,6 +6,41 @@ class GetEstimateModal extends Component{
 
     constructor(props){
         super(props);
+
+        this.state ={
+            name:'',
+            contactNumber:'',
+            emailId:'',
+            projectLocation:'',
+            message:''
+        };
+        this.handleName = this.handleName.bind(this);
+        this.handleContactNumber = this.handleContactNumber.bind(this);
+        this.handleEmailId = this.handleEmailId.bind(this);
+        this.handleProjectLocation = this.handleProjectLocation.bind(this);
+        this.handleMessage = this.handleMessage.bind(this);
+        this.sendEstimate = this.sendEstimate.bind(this);
+
+    }
+
+    handleName(e){
+        this.setState({name: e.target.value});
+    }
+    handleContactNumber(e){
+        this.setState({contactNumber: e.target.value});
+    }
+    handleEmailId(e){
+        this.setState({emailId: e.target.value});
+    }
+    handleProjectLocation(e){
+        this.setState({projectLocation: e.target.value});
+    } 
+    handleMessage(e){
+        this.setState({message: e.target.value});
+    }
+    sendEstimate(e){
+        e.preventDefault();
+        console.log(this.state);
     }
 
     render(){
@@ -18,16 +53,16 @@ class GetEstimateModal extends Component{
                         <img src={icClose} onClick={this.props.closeBtnClickChild}/>
 
                         <h5>GET AN ESTIMATE</h5>
-                        <form>
-                            <input type="text" name="personName" placeholder="NAME"></input> 
+                        <form onSubmit={this.sendEstimate}>
+                            <input type="text" name="personName" placeholder="NAME" value={this.state.name} onChange={this.handleName}></input> 
                             <br/>
-                            <input type="text" name="contactNumber" placeholder="CONTACT NUMBER"></input>
+                            <input type="text" name="contactNumber" placeholder="CONTACT NUMBER" value={this.state.contactNumber} onChange={this.handleContactNumber}></input>
                             <br/>
-                            <input type="text" name="emailId" placeholder="EMAIL ID"></input>
+                            <input type="text" name="emailId" placeholder="EMAIL ID" value={this.state.emailId} onChange={this.handleEmailId}></input>
                             <br/>
-                            <input type="text" name="projectLocation" placeholder="PROJECT LOCATION"></input>
+                            <input type="text" name="projectLocation" placeholder="PROJECT LOCATION" value={this.state.projectLocation} onChange={this.handleProjectLocation}></input>
                             <br/>
-                            <textarea name="message" cols="30" rows="3"  placeholder="MESSAGE"></textarea>
+                            <textarea name="message" cols="30" rows="3"  placeholder="MESSAGE" value={this.state.message} onChange={this.handleMessage}></textarea>
                             <br/>
                             <input type="submit" value="Submit"/>
                         </form>
