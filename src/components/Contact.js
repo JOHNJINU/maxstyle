1cc464f4ac6a7f579711fcd02b4928f99dfdbd0f
JOHNJINU <jinujohn09@gmail.com>
2021-03-17 14:59:57 +0530

    completed estimate and enquiry front end and backend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,97 @@
 import {Component} from 'react';
 import '../css/Contact.css';
 import icCarousel1 from '../img/ic_enquiry_bg.png';
-
 import AddressCard from './AddressCard';
+import axios from 'axios';
 
 class Contact extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state ={
+            name:'',
+            contactNumber:'',
+            emailId:'',
+            projectLocation:'',
+            message:''
+        };
+        this.handleName = this.handleName.bind(this);
+        this.handleContactNumber = this.handleContactNumber.bind(this);
+        this.handleEmailId = this.handleEmailId.bind(this);
+        this.handleProjectLocation = this.handleProjectLocation.bind(this);
+        this.handleMessage = this.handleMessage.bind(this);
+        this.sendCustomerEnquiry = this.sendCustomerEnquiry.bind(this);
+
+    }
+
+    handleName(e){
+        this.setState({name: e.target.value});
+    }
+    handleContactNumber(e){
+        this.setState({contactNumber: e.target.value});
+    }
+    handleEmailId(e){
+        this.setState({emailId: e.target.value});
+    }
+    handleProjectLocation(e){
+        this.setState({projectLocation: e.target.value});
+    } 
+    handleMessage(e){
+        this.setState({message: e.target.value});
+    }
+
+    sendCustomerEnquiry(e){
+        e.preventDefault();
+        console.log(this.state);
+
+        let estimateData = {
+            name:this.state.name,
+            contactNumber:this.state.contactNumber,
+            emailId:this.state.emailId,
+            projectLocation:this.state.projectLocation,
+            message:this.state.message
+        }
+        axios.post("http://192.168.64.2/maxstylebackend/customerEnquiry.php", estimateData).then(response => {
+
+        console.log(response);
+
+        let contactUsStatusMessage = document.querySelector(".enquiryStatusMessage");
+
+            if(response.data.isEnquirySaved==true){
+                
+                this.setState({
+                    name:'',
+                    contactNumber:'',
+                    emailId:'',
+                    projectLocation:'',
+                    message:''
+                });
+
+                // if previous attempt was failed statusFailure class will be there, so remove it
+                contactUsStatusMessage.classList.remove("statusFailure");
+
+                contactUsStatusMessage.classList.remove("d-none");
+                contactUsStatusMessage.classList.add("statusSuccess"); 
+                contactUsStatusMessage.innerText = "Thank you for contacting us! We will be in touch with you shortly" ;
+                
+                // after 2 seconds hide
+                //this.props.closeBtnClickChild();
+
+                setTimeout( ()=> {
+                    ///this.props.closeBtnClickChild();
+                    contactUsStatusMessage.classList.add("d-none");
+                }, 3000);
+
+            }else{
+                contactUsStatusMessage.classList.remove("d-none");
+                contactUsStatusMessage.classList.add("statusFailure"); 
+                contactUsStatusMessage.innerText = "Something went wrong. Please try again";
+            }
+
+        });
+    }
+
 
     render(){
         return(
@@ -26,21 +113,23 @@ class Contact extends Component{
 
                 {/* enquiry section starts */}
                 <div className="enquirySectionContainer">
+
                     <img className="imageEnquiry" src={icCarousel1} />
                     <div className="typingFieldContainer">
+                    <p className="enquiryStatusMessage d-none"></p> 
                         <h5>Enquiry</h5>
-                        <form>
-                            <input type="text" name="personName" placeholder="NAME"></input> 
+                        <form onSubmit={this.sendCustomerEnquiry}>
+                            <input type="text" name="personName" placeholder="NAME" value={this.state.name} onChange={this.handleName}  required></input> 
                             <br/>
-                            <input type="text" name="contactNumber" placeholder="CONTACT NUMBER"></input>
+                            <input type="text" name="contactNumber" placeholder="CONTACT NUMBER" value={this.state.contactNumber} onChange={this.handleContactNumber} required></input>
                             <br/>
-                            <input type="text" name="emailId" placeholder="EMAIL ID"></input>
+                            <input type="email" name="emailId" placeholder="EMAIL ID" value={this.state.emailId} onChange={this.handleEmailId} required></input>
                             <br/>
-                            <input type="text" name="projectLocation" placeholder="PROJECT LOCATION"></input>
+                            <input type="text" name="projectLocation" placeholder="PROJECT LOCATION" value={this.state.projectLocation} onChange={this.handleProjectLocation}></input>
                             <br/>
-                            <textarea name="message" cols="30" rows="4"  placeholder="MESSAGE"></textarea>
+                            <textarea name="message" cols="30" rows="4"  placeholder="MESSAGE" value={this.state.message} onChange={this.handleMessage}></textarea>
                             <br/>
-                            <input type="submit" value="Submit"/>
+                            <input type="submit" value="Submit" />
                         </form>
                     </div>
                 </div>
--- src/components/GetEstimateModal.js
+++ src/components/GetEstimateModal.js
@@ -1,6 +1,7 @@
 import {Component} from 'react';
 import '../css/GetEstimateModal.css';
 import icClose from '../img/ic_close.png';
+import axios from 'axios';
 
 class GetEstimateModal extends Component{
 
@@ -41,6 +42,53 @@ class GetEstimateModal extends Component{
     sendEstimate(e){
         e.preventDefault();
         console.log(this.state);
+
+        let estimateData = {
+            name:this.state.name,
+            contactNumber:this.state.contactNumber,
+            emailId:this.state.emailId,
+            projectLocation:this.state.projectLocation,
+            message:this.state.message
+        }
+        axios.post("http://192.168.64.2/maxstylebackend/getEstimate.php", estimateData).then(response => {
+
+        console.log(response);
+
+        let contactUsStatusMessage = document.querySelector(".contactUsStatusMessage");
+        let nonStatusItemsContainer = document.querySelector(".nonStatusItemsContainer");
+
+            if(response.data.isEstimateSaved==true){
+                
+                this.setState({
+                    name:'',
+                    contactNumber:'',
+                    emailId:'',
+                    projectLocation:'',
+                    message:''
+                });
+
+                // if previous attempt was failed statusFailure class will be there, so remove it
+                contactUsStatusMessage.classList.remove("statusFailure");
+
+                contactUsStatusMessage.classList.remove("d-none");
+                contactUsStatusMessage.classList.add("statusSuccess"); 
+                contactUsStatusMessage.innerText = "Thank you for contacting us! We will be in touch with you shortly" ;
+                nonStatusItemsContainer.classList.add("d-none");
+                
+                // after 2 seconds hide
+                //this.props.closeBtnClickChild();
+
+                setTimeout( ()=> {
+                    this.props.closeBtnClickChild();
+                }, 3000);
+
+            }else{
+                contactUsStatusMessage.classList.remove("d-none");
+                contactUsStatusMessage.classList.add("statusFailure"); 
+                contactUsStatusMessage.innerText = "Something went wrong. Please try again";
+            }
+
+        });
     }
 
     render(){
@@ -49,23 +97,27 @@ class GetEstimateModal extends Component{
                 {/* getEstimate section starts */}
                 <div className="getEstimateContainer">
                     <div className="getEstimateTypingContainer">
+                           
+                                <p className="contactUsStatusMessage d-none"></p> 
 
-                        <img src={icClose} onClick={this.props.closeBtnClickChild}/>
-
-                        <h5>GET AN ESTIMATE</h5>
-                        <form onSubmit={this.sendEstimate}>
-                            <input type="text" name="personName" placeholder="NAME" value={this.state.name} onChange={this.handleName}></input> 
-                            <br/>
-                            <input type="text" name="contactNumber" placeholder="CONTACT NUMBER" value={this.state.contactNumber} onChange={this.handleContactNumber}></input>
-                            <br/>
-                            <input type="text" name="emailId" placeholder="EMAIL ID" value={this.state.emailId} onChange={this.handleEmailId}></input>
-                            <br/>
-                            <input type="text" name="projectLocation" placeholder="PROJECT LOCATION" value={this.state.projectLocation} onChange={this.handleProjectLocation}></input>
-                            <br/>
-                            <textarea name="message" cols="30" rows="3"  placeholder="MESSAGE" value={this.state.message} onChange={this.handleMessage}></textarea>
-                            <br/>
-                            <input type="submit" value="Submit"/>
-                        </form>
+                                <div className="nonStatusItemsContainer">
+                                <img src={icClose} onClick={this.props.closeBtnClickChild}/>
+                                <h5>GET AN ESTIMATE</h5>
+                                <form onSubmit={this.sendEstimate}>
+                                    <input type="text" name="personName" placeholder="NAME" value={this.state.name} onChange={this.handleName} required></input> 
+                                    <br/>
+                                    <input type="text" name="contactNumber" placeholder="CONTACT NUMBER" value={this.state.contactNumber} onChange={this.handleContactNumber} required></input>
+                                    <br/>
+                                    <input type="email" name="emailId" placeholder="EMAIL ID" value={this.state.emailId} onChange={this.handleEmailId} required></input>
+                                    <br/>
+                                    <input type="text" name="projectLocation" placeholder="PROJECT LOCATION" value={this.state.projectLocation} onChange={this.handleProjectLocation}></input>
+                                    <br/>
+                                    <textarea name="message" cols="30" rows="3"  placeholder="MESSAGE" value={this.state.message} onChange={this.handleMessage}></textarea>
+                                    <br/>
+                                    <input type="submit" value="Submit"/>
+                                </form>
+                                </div>
+                            
                     </div>
                 </div>
                 {/* getEstimate section ends */}
